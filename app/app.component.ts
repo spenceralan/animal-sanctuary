7d360ff85bf962e08c5f01c7c9a6787f489fac73
Spencer Alan <spencer.alan@icloud.com>
2017-06-09 11:58:58 -0700

    connected app component

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Animal } from './animal';
+
+
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>Sanctuary City</h1>
+  `,
+})
+
+export class AppComponent {
+
+}
